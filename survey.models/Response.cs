42ffc5b406325a34d1e3c408227875b7579a3d47
phil.wheeler@intergen.co.nz <phil.wheeler@intergen.co.nz>
2018-02-09 16:15:39 +1300

    Add project files.

---
+++ survey.models/Response.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace survey.models
+{
+    public class Response
+    {
+        public int Id { get; set; }
+        public Guid Respondent { get; set; }
+        public DateTime Birthdate { get; set; }
+        public virtual Phone Phone { get; set; }
+        public virtual ICollection<SocialNetwork> SocialNetworks { get; set; }
+    }
+
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string OS { get; set; }
+    }
+
+    public class SocialNetwork
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
